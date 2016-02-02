0f1642a7a0f171a7fb1c423fc65642754c4f1a62
Paul <pfultz2@yahoo.com>
2016-02-01 18:14:14 -0600

    Add tests case and cmake

---
+++ test/case.cpp
@@ -0,0 +1,9 @@
+
+#include "test.h"
+
+PROVE_CASE()
+{
+    int i = 5;
+    PROVE_CHECK(i == 5);
+    // PROVE_CHECK(true);
+}
---
+++ test/test.h
@@ -0,0 +1,7 @@
+
+#include <prove.h>
+
+int main() 
+{
+    prove::run();
+}
